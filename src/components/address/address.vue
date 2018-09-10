aad5dd6424c584c104cbec4a50fd1731cfe95eba
Hot-Furnace <806228191@qq.com>
2018-09-10 20:09:54 +0800

    szj_first

--- src/components/address/address.vue
+++ src/components/address/address.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <h1>通讯录</h1>
+    <div class="main">
+        <div class="main-left">
+            <div class="header"></div>
+            <div class="sousou">
+                <input type="text">
+            </div>
+            <div class="neirong"></div>
+        </div>
+        <div class="main-right"></div>
     </div>
 </template>
 <script>
@@ -9,10 +16,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped >
-div{
-    width: 100%;
-    height: 100%;
+.main{
+    width: 1290px;
+    height: 393px;
     left: -1px;
-    background: blue;
+    // background: pink;
+    overflow: hidden;
 }
+
+.main-left{
+    width: 240px;
+    height: 393px;
+    background-color: yellow;
+    position: relative;
+}
+.main .main-left .header{
+    width: 240px;
+    height: 50px;
+    background-color:blue;
+    position: absolute;
+}
+.main .main-left .sousou{
+    width: 240px;
+    height: 46px;
+    background-color: green;
+}
+.main-right{
+    width: 1050px;
+    height: 393px;
+    background-color: pink;
+    float: left;
+}
+
 </style>
+
